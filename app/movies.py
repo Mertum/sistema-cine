75bce0bb5964abd5db1719aaa91886c96ab2af2c
leoarayav <leandro.araya08@inacapmail.cl>
2023-04-17 19:37:23 -0400

    add(constants/movies): movies object.

---
+++ app/movies.py
@@ -0,0 +1,29 @@
+"""
+    This file contains the movies in the system
+
+    NOTE: Movies are currently only for testing purposes :)
+"""
+
+MOVIES = [
+    {
+        "title": "The Shawshank Redemption",
+        "rating": "9.3",
+        "duration": "2h 22m",
+        "seats": 100,
+        "price": 22.00
+    },
+    {
+        "title": "The Godfather",
+        "rating": "9.2",
+        "duration": "2h 55m",
+        "seats": 50,
+        "price": 20.00
+    },
+    {
+        "title": "The Dark Knight",
+        "rating": "9.0",
+        "duration": "2h 32m",
+        "seats": 100,
+        "price": 18.00
+    }
+]
