8771d15af688a9afe773edf0973f875860c975f7
leoarayav <leandro.araya08@inacapmail.cl>
2023-04-19 13:27:45 -0400

    chore(*): added docstrings

--- app/cinema.py
+++ app/cinema.py
@@ -6,27 +6,57 @@ from .movie import Movie
 from .constants import Constants
 
 class Cinema:
-    """The Cinema class which contains the cinema system"""
+    """
+    The Cinema class which contains the cinema system
+
+    Attributes:
+        movies (list[Movie]): A list of all the available movies
+        tickets (list[Ticket]): A list of all the bought tickets
+        seats (dict[str, list[Seat]]): A dictionary of all the available seats for each ticket type
+    """
 
     def __init__(self) -> None:
         self.movies = Constants.MOVIES
         self.tickets = []
         self.seats = { 
-            "Regular": Constants.REGULAR_SEATS, 
-            "VIP": Constants.VIP_SEATS, 
-            "3D": Constants.SEATS_3D 
+            "regular": Constants.REGULAR_SEATS, 
+            "vip": Constants.VIP_SEATS, 
+            "3d": Constants.SEATS_3D 
         }
     
     def get_movies(self) -> list[Movie]:
-        """Returns a list of all the available movies"""
+        """
+        Returns a list of all the available movies
+
+        Args:
+            This method doesn't take any arguments
+        
+        Returns: -> list[Movies]
+        """
         return self.movies
     
     def get_seats(self, ticket_type: str) -> list[Seat]:
-        """Returns a list of all the available seats for a given ticket type"""
+        """
+        This method show the list of all the available seats for a given ticket type
+
+        Args:
+            ticket_type (str): The ticket type
+        
+        Returns: -> list[Seat]
+        """
         return self.seats[ticket_type]
 
     def buy_ticket(self, movie_name: str, ticket_type: str, seat_num: int) -> Ticket:
-        """This method allow buys a ticket for a movie"""
+        """
+        This method allow buys a ticket for a movie
+
+        Args:
+            movie_name (str): The movie name
+            ticket_type (str): The ticket type
+            seat_num (int): The seat number
+        
+        Returns: -> Ticket
+        """
         movie = None
         for m in self.movies:
             if m.name == movie_name:
--- app/movie.py
+++ app/movie.py
@@ -10,21 +10,47 @@ class Movie:
         self.__showtimes = []
     
     def __str__(self) -> str:
-        """The string representation of the Movie object"""
+        """
+        The string representation of the Movie object
+
+        Returns: -> str
+        """
         return f"({self.__class__.__name__}) -> {self.name} - {self.duration} minutes"
     
     def __repr__(self) -> str:
-        """The representation of the Movie object"""
+        """
+        The representation of the Movie object
+        
+        Returns: -> str
+        """
         return self.__str__()
     
     def __eq__(self, other: object) -> bool:
-        """Checks if two Movie objects are equal"""
+        """
+        Checks if two Movie objects are equal
+
+        Args:
+            other (object): The other object to compare with.
+        
+        Returns: -> bool
+        """
         return self.name == other.name
     
     def add_showtime(self, showtime: str) -> None:
-        """Adds a showtime to the movie"""
+        """
+        Adds a showtime to the movie
+
+        Args:
+            showtime (str): The showtime to add
+        
+        Returns: -> None
+        """
         self.__showtimes.append(showtime)
 
     def get_showtimes(self) -> list:
-        """Returns a list of the showtimes for the movie"""
+        """
+        Returns a list of the showtimes for the movie
+
+        Returns: -> list
+        """
         return self.__showtimes
